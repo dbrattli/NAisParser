951ea5cb08d6cbc55a2cd252c1082aa58fc27e45
Dag Brattli <dag@brattli.net>
2018-04-29 08:27:03 +0200

    Return inner result for success and raise exception for error

--- parser/Api.fs
+++ parser/Api.fs
@@ -4,11 +4,13 @@ open System
 open FParsec
 open AisParser.Ais
 
-[<AutoOpen>]
-module Api =
 
-    type public Parser() =
+type public Parser() =
 
-        member this.Parse(input: String) =
-            let result = run aisParser input
-            result
+    member public this.Parse(input: String) =
+        let result = run aisParser input
+        match result with 
+        | Success (c, state, pos) ->
+            c
+        | Failure (a, b, c)  ->
+            raise (System.ArgumentException(a))
