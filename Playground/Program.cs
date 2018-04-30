c82b47d6870323b85de2c593caedbfe2f665169d
Dag Brattli <dag@brattli.net>
2018-04-30 17:36:11 +0200

    Added playground project

---
+++ Playground/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using AisParser;
+
+namespace Kystverket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            AisResult aisResult;
+            var result = parser.TryParse("!BSVDM,1,1,,A,13mAwp001m0MMrjSoomG6mWT0<1h,0*16", out aisResult);
+            //result.
+            Console.WriteLine(aisResult.Channel.ToString());
+
+            result = parser.TryParse(aisResult, out CommonNavigationBlockResult cnbResult);
+            Console.WriteLine(cnbResult.ToString());
+            Console.ReadLine();
+        }
+    }
+}
