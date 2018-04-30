3764e3cbe64f2debe3af5d5284ee77c5f507fb8c
Dag Brattli <dag@brattli.net>
2018-04-30 07:19:43 +0200

    Trying to decide on the api. wip

--- parser/Api.fs
+++ parser/Api.fs
@@ -1,15 +1,20 @@
 namespace AisParser
 
 open System
+open System.Collections.Generic
+
 open FParsec
 open AisParser.Ais
 
 
 type public Parser() =
 
+    let fragments = new List<ParserResult<AisResult, unit>>()
+
     member public this.Parse(input: String) =
         let result = run aisParser input
-        match result with 
+
+        match result with
         | Success (c, state, pos) ->
             c
         | Failure (a, b, c)  ->
