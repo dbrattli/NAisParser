4c39306a1f7a82508eaeee1c061ca999b0c3915e
Dag Brattli <dag@brattli.net>
2018-04-30 07:47:58 +0200

    API wip

--- parser/Api.fs
+++ parser/Api.fs
@@ -7,15 +7,21 @@ open FParsec
 open AisParser.Ais
 
 
-type public Parser() =
+type public Parser(cb : Action<ParserResult<AisResult, unit>>) =
+
+    let callback = cb
 
     let fragments = new List<ParserResult<AisResult, unit>>()
 
-    member public this.Parse(input: String) =
+    member public this.Parse(input: String) : unit =
         let result = run aisParser input
 
         match result with
         | Success (c, state, pos) ->
-            c
+            if c.Number < c.Count then
+                fragments.Add(result)
+            else
+                callback.Invoke(result)
+
         | Failure (a, b, c)  ->
             raise (System.ArgumentException(a))
