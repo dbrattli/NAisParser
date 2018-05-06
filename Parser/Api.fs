a7b905b784a154b4e1c3b1dadfae7a79b03b9327
Dag Brattli <dag@brattli.net>
2018-05-06 16:20:41 +0200

    Added XML documents to public methods

--- Parser/Api.fs
+++ Parser/Api.fs
@@ -11,6 +11,22 @@ open System.Runtime.InteropServices //for OutAttribute
 type public Parser() =
     let fragments = new List<ParserResult<AisResult, unit>>()
 
+    /// <summary>
+    /// Parses the AIS data packet outer layer. This is the first stage
+    /// of parsing a VDM/VDO data packet.
+    /// </summary>
+    /// <param name="input">A string to parse.</param>
+    /// <param name="result">
+    /// An AisResult that will be set if the method returns true.
+    /// </param>
+    /// <returns>
+    /// True if parsing of the packet completed, and the AisResult is valid.
+    /// False if the packet is fragmented. If false then this method needs
+    /// to be called again with the next packet fragment.
+    /// </returns>
+    /// <exception cref="System.ArgumentException">Thrown if the data
+    /// packet is invalid and parsing of the packet failed.
+    /// </exception>
     member public this.TryParse(input: String, [<Out>] result : AisResult byref) : bool =
         let res = run aisParser input
 
@@ -32,6 +48,22 @@ type public Parser() =
         | Failure (error, _, _)  ->
             raise (System.ArgumentException(error))
 
+    /// <summary>
+    /// Parses an AIS data packet inner layer of message type 1, 2 or 3.
+    /// This is the second stage of parsing a VDM/VDO data packet.
+    /// </summary>
+    /// <param name="input">An AisResult to parse the payload of.</param>
+    /// <param name="result">
+    /// A MessageType123 that will be set if the method returns true.
+    /// </param>
+    /// <returns>
+    /// True if parsing of the packet completed. False if this is not
+    /// a message of type 1, 2, or 3.
+    /// </returns>
+    /// <exception cref="System.ArgumentException">Thrown if the data
+    /// payload is invalid and parsing of the packet failed.
+    /// </exception>
+
     member public this.TryParse(input: AisResult, [<Out>] result : MessageType123 byref) : bool =
         let binaryString = Common.intListToBinaryString input.Payload
         let res = run Type123.parseMessageType123 binaryString
@@ -46,6 +78,22 @@ type public Parser() =
                 false
         | Failure (error, _, _)  ->
             raise (System.ArgumentException(error))
+
+    /// <summary>
+    /// Parses an AIS data packet inner layer of message type 5.
+    /// This is the second stage of parsing a VDM/VDO data packet.
+    /// </summary>
+    /// <param name="input">An AisResult to parse the payload of.</param>
+    /// <param name="result">
+    /// A MessageType5 that will be set if the method returns true.
+    /// </param>
+    /// <returns>
+    /// True if parsing of the packet completed. False if this is not
+    /// a message of type 5.
+    /// </returns>
+    /// <exception cref="System.ArgumentException">Thrown if the data
+    /// payload is invalid and parsing of the packet failed.
+    /// </exception>
 
     member public this.TryParse(input: AisResult, [<Out>] result : MessageType5 byref) : bool =
         let binaryString = Common.intListToBinaryString input.Payload
