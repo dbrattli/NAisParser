c3b5291db37d5923917e9d4b960401cee2106c58
Dag Brattli <dag@brattli.net>
2018-10-01 23:49:45 +0200

    Added F# api

---
+++ Examples/NcaFeed/Program.fs
@@ -0,0 +1,58 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.IO
+open System.Net.Sockets
+
+open NAisParser
+
+let parser = new Parser()
+
+let rec asyncResponse (reader : StreamReader) =
+    async {
+        let! line = reader.ReadLineAsync () |> Async.AwaitTask
+        let result = parser.Parse line
+        match result with
+        | Ok aisResult ->
+            match aisResult.Type with
+            | 1uy | 2uy | 3uy ->
+                let res = parser.ParseType123 aisResult
+                match res with
+                | Result.Ok msg ->
+                    printf "%A" (msg.ToString ())
+                | Result.Error err -> ()
+            | 4uy ->
+                let res = parser.ParseType4 aisResult
+                match res with
+                | Result.Ok msg ->
+                    printf "%A" (msg.ToString ())
+                | Result.Error err -> ()
+            | 5uy ->
+                let res = parser.ParseType5 aisResult
+                match res with
+                | Result.Ok msg ->
+                    printf "%A" (msg.ToString ())
+                | Result.Error err -> ()
+            | _ ->
+                ()
+        | Error err ->
+            printfn "Got exception: %A" (err.ToString ())
+        | Continue ->
+            ()
+
+        return! asyncResponse reader
+    }
+
+[<EntryPoint>]
+let main args =
+    let port = 5631
+    let server = "153.44.253.27"
+
+    let client = new TcpClient ()
+    client.Connect (server, port)
+    printfn "Connected to %A %A..." server port
+    let stream = client.GetStream ()
+    use reader = new StreamReader (stream)
+
+    printfn "Got stream, starting asynchronous communication."
+    asyncResponse reader |> Async.RunSynchronously
+    0
--- Source/Api.fs
+++ Source/Api.fs
@@ -12,6 +12,13 @@ type MessageType =
     | Type4 of MessageType4
     | Type5 of MessageType5
 
+exception ParseException of string
+
+type ParseResult<'a> =
+    | Ok of 'a
+    | Error of exn
+    | Continue
+
 type public Parser() =
     let fragments = new List<ParserResult<AisResult, unit>>()
 
@@ -31,7 +38,7 @@ type public Parser() =
     /// <exception cref="System.ArgumentException">Thrown if the data
     /// packet is invalid and parsing of the packet failed.
     /// </exception>
-    member public _this.TryParse(input: String, [<Out>] result : AisResult byref) : bool =
+    member public this.TryParse(input: String, [<Out>] result : AisResult byref) : bool =
         let res = run aisParser input
 
         match res with
@@ -67,8 +74,7 @@ type public Parser() =
     /// <exception cref="System.ArgumentException">Thrown if the data
     /// payload is invalid and parsing of the packet failed.
     /// </exception>
-
-    member public _this.TryParse(input: AisResult, [<Out>] result : MessageType123 byref) : bool =
+    member public this.TryParse(input: AisResult, [<Out>] result : MessageType123 byref) : bool =
         let binaryString = Common.intListToBinaryString input.Payload
         let res = run Type123.parseMessageType123 binaryString
 
@@ -93,8 +99,7 @@ type public Parser() =
     /// <exception cref="System.ArgumentException">Thrown if the data
     /// payload is invalid and parsing of the packet failed.
     /// </exception>
-
-    member public _this.TryParse(input: AisResult, [<Out>] result : MessageType4 byref) : bool =
+    member public this.TryParse(input: AisResult, [<Out>] result : MessageType4 byref) : bool =
         let binaryString = Common.intListToBinaryString input.Payload
         let res = run Type4.parseMessageType4 binaryString
 
@@ -119,8 +124,7 @@ type public Parser() =
     /// <exception cref="System.ArgumentException">Thrown if the data
     /// payload is invalid and parsing of the packet failed.
     /// </exception>
-
-    member public _this.TryParse(input: AisResult, [<Out>] result : MessageType5 byref) : bool =
+    member public this.TryParse(input: AisResult, [<Out>] result : MessageType5 byref) : bool =
         let binaryString = Common.intListToBinaryString input.Payload
         let res = run Type5.parseMessageType5 binaryString
 
@@ -130,3 +134,67 @@ type public Parser() =
             true
         | Failure (error, _, _)  ->
             raise (System.ArgumentException(error))
+
+    /// <summary>
+    /// Parses the AIS data packet outer layer. This is the first stage
+    /// of parsing a VDM/VDO data packet.
+    /// </summary>
+    /// <param name="input">A string to parse.</param>
+    /// <param name="result">
+    /// An AisResult that will be set if the method returns true.
+    /// </param>
+    /// <returns>
+    /// True if parsing of the packet completed, and the AisResult is valid.
+    /// False if the packet is fragmented. If false then this method needs
+    /// to be called again with the next packet fragment.
+    /// </returns>
+    member public this.Parse (input: String) : ParseResult<AisResult> =
+        let res = run aisParser input
+
+        match res with
+        | Success (c, _, _) ->
+            fragments.Add(res)
+
+            if c.Number < c.Count then
+                Continue
+            else
+                let fragment = fragments |> Seq.reduce defragment
+                match fragment with
+                | Success (c, _, _) ->
+                    let res = Ok { c with Type = byte c.Payload.[0]; Payload = c.Payload }
+                    fragments.Clear()
+                    res
+                | Failure (error, _, _)  ->
+                    Error (ParseException error)
+        | Failure (error, _, _)  ->
+            Error (ParseException error)
+
+    member public this.ParseType123(input: AisResult) : Result<MessageType123, exn> =
+        let binaryString = Common.intListToBinaryString input.Payload
+        let res = run Type123.parseMessageType123 binaryString
+
+        match res with
+        | Success (message, _, _) ->
+            Result.Ok message
+        | Failure (error, err, _)  ->
+            ParseException error |> Result.Error
+
+    member public this.ParseType4(input: AisResult) : Result<MessageType4, exn> =
+        let binaryString = Common.intListToBinaryString input.Payload
+        let res = run Type4.parseMessageType4 binaryString
+
+        match res with
+        | Success (message, _, _) ->
+            Result.Ok message
+        | Failure (error, _, _)  ->
+            ParseException error |> Result.Error
+
+    member public this.ParseType5(input: AisResult) : Result<MessageType5, exn> =
+        let binaryString = Common.intListToBinaryString input.Payload
+        let res = run Type5.parseMessageType5 binaryString
+
+        match res with
+        | Success (message, _, _) ->
+            Result.Ok message
+        | Failure (error, err, _)  ->
+            ParseException error |> Result.Error
